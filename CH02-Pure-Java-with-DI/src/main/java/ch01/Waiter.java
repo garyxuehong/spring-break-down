3e9b707e3dac1dd95a83d6ef01bdf30317d1346b
Gary Xue <gary.xuehong@gmail.com>
2023-11-01 21:16:13 +1100

    add url

--- CH02-Pure-Java-with-DI/src/main/java/ch01/Waiter.java
+++ CH02-Pure-Java-with-DI/src/main/java/ch01/Waiter.java
@@ -3,6 +3,12 @@ package ch01;
 public class Waiter {
     private Kiosk kiosk;
 
+    /*
+        Dependency Injection:
+        Wiki: https://en.wikipedia.org/wiki/Dependency_injection#:~:text=In%20software%20engineering%2C%20dependency%20injection,opposed%20to%20creating%20them%20internally.
+        Angular (frontend): https://angular.io/guide/dependency-injection
+        Medium: https://medium.com/javarevisited/all-you-need-to-know-about-dependency-injection-c8f896e08993
+     */
     public Waiter(Kiosk kiosk) {
         this.kiosk = kiosk;
     }
